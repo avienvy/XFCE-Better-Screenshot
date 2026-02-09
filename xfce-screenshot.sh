8c07821f4194a09b38b8967433c73e02d05cd4c5
avienvy <78303735+avienvy@users.noreply.github.com>
2026-02-09 13:32:48 +0700

    Update xfce-screenshot.sh

--- xfce-screenshot.sh
+++ xfce-screenshot.sh
@@ -2,8 +2,14 @@
 
 DIR="$HOME/Pictures"
 FILENAME="Screenshot_$(date +%Y-%m-%d_%H-%M-%S).jpg"
+FILEPATH="$DIR/$FILENAME"
 # -f: Take screenshot of full screen
-# -u: Take screenshot of active window 
+# -w: Take screenshot of active window 
 # -r: Take screenshot of selected screen (partially)
-xfce4-screenshooter -f -s "$DIR/$FILENAME"
-notify-send "Screenshot Saved" "$DIR/$FILENAME" -t 1200
+xfce4-screenshooter -f -s "$FILEPATH"
+
+if [ -f "$FILEPATH" ]; then
+    notify-send "Screenshot Saved" "$FILEPATH" -t 1200
+else
+    notify-send "Screenshot Cancelled" -t 1200
+fi
